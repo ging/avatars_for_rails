c73c95c9415d6bbda9b6c5afbf07421e20aed886
Jaime Castro Montero <jaime1988@gmail.com>
2011-04-15 12:17:32 +0200

    tab fixed

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -1,34 +1,32 @@
 #class AvatarsController < InheritedResources::Base
 class AvatarsController < ApplicationController
-
-
   def index
     @avatars = Avatar.all
 
     respond_to do |format|
       format.html # index.html.erb
-      #format.xml { render :xml => @gastos }
+    #format.xml { render :xml => @gastos }
     end
   end
-  
+
   def new
     @avatars = Avatar.all
     @avatar = Avatar.create(params[:avatar])
-    
+
     if params[:drag_name].blank?
       respond_to do |format|
         format.html # new.html.erb
       end
     else
-      #debugger
+    #debugger
       if Avatar.check_image_tmp_valid(params[:drag_name].to_s)
         render :partial => 'precrop'
       else
         @avatar.logo.errors['invalid_type'] = I18n.t('avatar.error.no_image_file')
         render :partial => 'errors'
-      end 
+      end
     end
-    
+
   end
 
   def update
@@ -51,22 +49,22 @@ class AvatarsController < ApplicationController
           old_logo_id = params[:id].to_s
         end
 
-      new_logo.active = true
-      new_logo.save
-      
-      respond_to do |format|
-        format.js { render :layout=>false , :locals=>{:new_id => params[:id],:old_id => old_logo_id}}
-      end
-      
+        new_logo.active = true
+        new_logo.save
+
+        respond_to do |format|
+          format.js { render :layout=>false , :locals=>{:new_id => params[:id],:old_id => old_logo_id}}
+        end
+
       end
     end
 
-   # redirect_to avatars_path
+  # redirect_to avatars_path
   end
 
   def create
     @avatar = Avatar.create(params[:avatar])
-    
+
     if @avatar.new_record?
       if (params[:avatar].blank? || params[:avatar][:drag].nil?)
         render :new
@@ -74,41 +72,41 @@ class AvatarsController < ApplicationController
         render :json => {:name => File.basename(@avatar.logo.queued_for_write[:original].path) }
       end
     else
-      #debugger
+    #debugger
       @avatar.updating_logo = true
-      @avatar.actor_id = current_avatarable_object.id
+      @avatar.avatarable.id = current_avatarable_object.id
       unless current_avatarable_object.avatars.blank?
         current_avatarable_object.avatars.each do |old_logo|
           old_logo.active = false
           old_logo.save
         end
-        #actual_logo = current_avatarable_object.avatars.active.first
-        #actual_logo.active = false
-        #actual_logo.save
+      #actual_logo = current_avatarable_object.avatars.active.first
+      #actual_logo.active = false
+      #actual_logo.save
       end
-      
+
       @avatar.active = true
       @avatar.save
       redirect_to avatars_path
-    
+
     end
   end
 
   def destroy
     @avatar = Avatar.find(params[:id])
-    
-    if (@avatar.actor == current_avatarable_object)
+
+    if (@avatar.avatarable == current_avatarable_object)
       @avatar.destroy
       respond_to do |format|
         format.js { render :layout=>false , :locals=>{:deleted_id => params[:id]}}
       end
     end
-    #redirect_to avatars_path
+  #redirect_to avatars_path
 
   end
 
-  def current_avatarable_object
-    return Actor.find(:all).first
-  end
+  #def current_avatarable_object
+    #return Actor.find(:all).first
+  #end
 
 end
--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -1,123 +1,120 @@
 require 'RMagick'
 
 class Avatar < ActiveRecord::Base
- 	has_attached_file :logo,
+  has_attached_file :logo,
                       :styles => { :representation => "20x20>",
                                    :tie => "30x30>",
                                    :actor => '35x35>',
                                    :profile => '94x94' },
                       :default_url => "/images/logos/:style/:subtype_class.png"
-	
-	before_post_process :process_precrop
-	attr_accessor :crop_x, :crop_y, :crop_w, :crop_h, :name,:updating_logo,:drag,:drag_name
-	validates_attachment_presence :logo, :if => :uploading_file?, :message => I18n.t('avatar.error.no_file')
-		
-	after_validation :precrop_done
-	
-	#belongs_to :actor
-	belongs_to AvatarsForRails.avatarable_model
-	
-	scope :active, where(:active => true)
 
-        def avatarable
-          __send__ AvatarsForRails.avatarable_model # __send__ :actor
-        end
-	
-  	def uploading_file?
-    	return @name.blank?
-  	end
-	
-	def precrop_done
-		return if @name.blank? || !@updating_logo.blank?
+  before_post_process :process_precrop
+  attr_accessor :crop_x, :crop_y, :crop_w, :crop_h, :name,:updating_logo,:drag,:drag_name
+  validates_attachment_presence :logo, :if => :uploading_file?, :message => I18n.t('avatar.error.no_file')
 
-    	precrop_path = File.join(Avatar.images_tmp_path,@name)
-    	
-    	make_precrop(precrop_path,@crop_x.to_i,@crop_y.to_i,@crop_w.to_i,@crop_h.to_i)
-	   	@avatar = Avatar.new :logo => File.open(precrop_path), :name => @name
-		
-				
-		self.logo = @avatar.logo
-		
-		FileUtils.remove_file(precrop_path)
-	end
-	
-	def self.images_tmp_path
-		images_path = File.join(RAILS_ROOT, "public", "images")
-		tmp_path = FileUtils.mkdir_p(File.join(images_path, "tmp"))		
-	end
-	
-	def self.copy_to_temp_file(path)
-		FileUtils.cp(path,Avatar.images_tmp_path)
-	end	
-	
-	
-	def self.get_image_dimensions(name)
-   		img_orig = Magick::Image.read(name).first
-   		dimensions = {}
-   		dimensions[:width] =  img_orig.columns
-   		dimensions[:height] = img_orig.rows
-   		dimensions
-   end
-	   
-	 def self.check_image_tmp_valid(path)
-     
-     begin
+  after_validation :precrop_done
+
+  #belongs_to :actor
+  belongs_to AvatarsForRails.avatarable_model
+
+  scope :active, where(:active => true)
+  
+  def avatarable
+    __send__ AvatarsForRails.avatarable_model # __send__ :actor
+  end
+
+  def uploading_file?
+    return @name.blank?
+  end
+
+  def precrop_done
+    return if @name.blank? || !@updating_logo.blank?
+
+    precrop_path = File.join(Avatar.images_tmp_path,@name)
+
+    make_precrop(precrop_path,@crop_x.to_i,@crop_y.to_i,@crop_w.to_i,@crop_h.to_i)
+    @avatar = Avatar.new :logo => File.open(precrop_path), :name => @name
+
+    self.logo = @avatar.logo
+
+    FileUtils.remove_file(precrop_path)
+  end
+
+  def self.images_tmp_path
+    images_path = File.join(RAILS_ROOT, "public", "images")
+    tmp_path = FileUtils.mkdir_p(File.join(images_path, "tmp"))
+  end
+
+  def self.copy_to_temp_file(path)
+    FileUtils.cp(path,Avatar.images_tmp_path)
+  end
+
+  def self.get_image_dimensions(name)
+    img_orig = Magick::Image.read(name).first
+    dimensions = {}
+    dimensions[:width] =  img_orig.columns
+    dimensions[:height] = img_orig.rows
+    dimensions
+  end
+
+  def self.check_image_tmp_valid(path)
+
+    begin
       path = File.join(Avatar.images_tmp_path,path)
       img_orig = Magick::Image.read(path).first
       return true
-     rescue
-       return false
-     end
-   end
-   
-   def process_precrop
-   	
-  	if @name.blank? && (  logo.content_type.present? && !logo.content_type.start_with?("image/"))
-		  logo.errors['invalid_type'] = I18n.t('avatar.error.no_image_file')
-		  return false
-	  end
-   	  	
-	  return if !@name.blank?
+    rescue
+    return false
+    end
+  end
 
-      if resize_image(logo.queued_for_write[:original].path,500,500)
-        logo.errors['precrop'] = "You have to make precrop"
-        Avatar.copy_to_temp_file(logo.queued_for_write[:original].path)
-      else
-        
-      end
-   end
-      
-   def resize_image(path,width,height)
-     begin
+  def process_precrop
+
+    if @name.blank? && (  logo.content_type.present? && !logo.content_type.start_with?("image/"))
+      logo.errors['invalid_type'] = I18n.t('avatar.error.no_image_file')
+    return false
+    end
+
+    return if !@name.blank?
+
+    if resize_image(logo.queued_for_write[:original].path,500,500)
+      logo.errors['precrop'] = "You have to make precrop"
+      Avatar.copy_to_temp_file(logo.queued_for_write[:original].path)
+    else
+
+    end
+  end
+
+  def resize_image(path,width,height)
+    begin
       img_orig = Magick::Image.read(path).first
       img_orig = img_orig.resize_to_fit(width, height)
       img_orig.write(path)
       return true
-     rescue
-       logo.errors['invalid_type'] = I18n.t('avatar.error.no_image_file')
-       return false
-     end
-   end
-   
-   def make_precrop(path,x,y,width,height)
-     begin
+    rescue
+      logo.errors['invalid_type'] = I18n.t('avatar.error.no_image_file')
+    return false
+    end
+  end
+
+  def make_precrop(path,x,y,width,height)
+    begin
       img_orig = Magick::Image.read(path).first
-     rescue
-       logo.errors['invalid_type'] = I18n.t('avatar.error.no_image_file')
-       return false
-     end
-      
-     dimensions = Avatar.get_image_dimensions(path)
-     
-	   unless (width == 0) || (height == 0)
-		  crop_args = [x,y,width,height]
-      img_orig = img_orig.crop(*crop_args)
-	   end     
- 
-     img_orig = img_orig.resize_to_fill(500,500)
-     
-     
-     img_orig.write(path)
-  end   
+    rescue
+      logo.errors['invalid_type'] = I18n.t('avatar.error.no_image_file')
+    return false
+    end
+
+    dimensions = Avatar.get_image_dimensions(path)
+
+    unless (width == 0) || (height == 0)
+      crop_args = [x,y,width,height]
+    img_orig = img_orig.crop(*crop_args)
+    end
+
+    img_orig = img_orig.resize_to_fill(500,500)
+
+    img_orig.write(path)
+  end
 end
 
