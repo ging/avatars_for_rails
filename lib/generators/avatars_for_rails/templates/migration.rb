82f4c218004c85c36b6d4ee68b2a8ffdaacb8aed
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2013-03-07 17:48:41 +0100

    Fix migration

--- lib/generators/avatars_for_rails/templates/migration.rb
+++ lib/generators/avatars_for_rails/templates/migration.rb
@@ -1,20 +1,9 @@
 class CreateAvatarsForRails<%= file_name.camelize %> < ActiveRecord::Migration
-  def self.up    
-  #Tables
-    create_table "avatars", :force => true do |t|
-      t.integer  "<%= file_name %>_id" 
-      t.string   "logo_file_name"
-      t.string   "logo_content_type"
-      t.integer  "logo_file_size"
-      t.datetime "logo_updated_at"
-      t.boolean  "active", :default => true
-    end
-    
-    add_index "avatars", "<%= file_name %>_id"
-    
+  def up    
+    add_attachment :<%= file_name.tableize %>, :logo
   end
   
   def self.down
-    drop_table :avatars
+    remove_attachment :<%= file_name.tableize %>, :logo
   end
 end
